d5bab2b3bd41e6f4a4bbb1a3f3cf99ce9e935c82
tpham13 <tpham0123@gmail.com>
2020-06-05 22:51:09 -0700

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+    attr_accessor :first_name, :last_name
+    # @@all = []
+    # def intialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name 
+    #     @@all << self 
+    # end 
+end 
